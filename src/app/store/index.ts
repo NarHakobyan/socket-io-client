260613ed6e767ae03d2d230ddc75108fc655b12b
Narek <narek.hakobyan.07@gmail.com>
2017-12-30 17:05:14 +0400

    feature: moved store object to separate directory

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -4,6 +4,3 @@ export interface AppState {
   progressBar: ProgressBarReducer.ProgressBarState;
   socketIo: SocketIoReducer.SocketIoState;
 }
-
-export const getProgressBarState = (state: AppState) => state.progressBar;
-export const getSocketIoState = (state: AppState) => state.socketIo;
