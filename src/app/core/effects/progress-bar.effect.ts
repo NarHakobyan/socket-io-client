ead27f775a7832d02259faea585074fdd37a4fa1
Narek <narek.hakobyan.07@gmail.com>
2017-12-30 17:27:15 +0400

    fix: removed progress bar effects

--- src/app/core/effects/progress-bar.effect.ts
+++ src/app/core/effects/progress-bar.effect.ts
@@ -1,27 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
-import { ProgressBarActions } from 'app/core/actions/index';
-import { Observable } from 'rxjs/Observable';
+import { Actions } from '@ngrx/effects';
 
 
 @Injectable()
 export class ProgressBarEffect {
-
-  @Effect() toggle: Observable<ProgressBarActions.All> = this.actions$
-    .ofType<ProgressBarActions.All>(ProgressBarActions.SHOW, ProgressBarActions.HIDE).distinctUntilChanged()
-    .do<ProgressBarActions.All>(action => {
-      console.log(action);
-    });
-  @Effect() show: Observable<ProgressBarActions.Show> = this.actions$
-    .ofType<ProgressBarActions.Show>(ProgressBarActions.SHOW).distinctUntilChanged()
-    .do<ProgressBarActions.Show>(action => {
-      console.log(action);
-    });
-  @Effect() hide: Observable<ProgressBarActions.Hide> = this.actions$
-    .ofType<ProgressBarActions.Hide>(ProgressBarActions.HIDE).distinctUntilChanged()
-    .do<ProgressBarActions.Hide>(action => {
-      console.log(action);
-    });
 
   constructor(private actions$: Actions) {
   }
