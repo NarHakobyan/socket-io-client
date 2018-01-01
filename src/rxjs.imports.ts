338fd7ed073152a87a739ae960812aa9c5569d91
Narek <narek.hakobyan.07@gmail.com>
2018-01-01 23:44:02 +0400

    fix: added necessary rxjs operators

--- src/rxjs.imports.ts
+++ src/rxjs.imports.ts
@@ -11,3 +11,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/takeLast';
+import 'rxjs/add/operator/withLatestFrom';
